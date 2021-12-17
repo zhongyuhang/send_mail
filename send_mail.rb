5f933b56789ee47e26bd818c762b92a75256f7bc
zhongyuhang <1076525788@qq.com>
2021-12-17 10:57:11 +0800

    update

--- send_mail.rb
+++ send_mail.rb
@@ -1,43 +1,140 @@
 require 'mail'
 require 'active_record'
 
+LOG_ERROR_TYPE = []
+BASE_LOG_PATH = "/home/yuhang/.kube/log/"
+WORKER_LOG_FILE_NAME = [
+  'factual_data_to_avvoproduction.log',
+  'directory_attorney_availability_production.log',
+  'kafka_consumers_production.log',
+]
 class LocalTable < ActiveRecord::Base
   self.pluralize_table_names  = false
   self.abstract_class         = true
   def failed_type
-    return if self.status != 'failed'
-    log = self.class.table_name == "lawyer_match_request" ? self.latest_log.message : self.process_log
+    log = self.class.table_name == "lawyer_match_request_log" ? self.message : self.process_log
     failed_type = [
       'Temporary failure in name resolution',
-      'Response code = 503',
-      'Response code = 502',
-      'Response code = 500',
-      'Response code = 404',
+      'Response code = (\d{3})',
       'unknown attribute `primary_persona_flag_v2`',
+      'Access denied for user',
+      'bad URI',
     ]
-    type = failed_type.find{|e| /#{e}/ =~ log}
+    type = failed_type.find{|e| /#{e}/i =~ log}
+    type = "Response code = #{$1}" if type == 'Response code = (\d{3})'
     return type.nil? ? "unclassified errors" : type
   end
 end
-
 class LawyerMatchRequest < LocalTable
   has_many :lawyer_match_request_logs
   def latest_log
     self.lawyer_match_request_logs.sort_by(&:updated_at).last
   end
 end
-
 class LawyerMatchRequestLog < LocalTable
   belongs_to :lawyer_match_request
 end
-
 class LmbRequest < LocalTable ; end
-
 class ForagerRequest < LocalTable ; end
-
 ActiveRecord::Base.establish_connection(YAML.load_file("db.yml"))
 
-def set_tbody(objects)
+def send_mail
+  # cp_file_from_pod_to_local
+  smtp = { 
+    :address => "smtp.gmail.com",
+    :port => 587, 
+    :domain => "gmail.com",
+    :user_name => "autoforager.avvo@gmail.com",
+    :password => "avvo.com2",
+    :enable_starttls_auto => true
+  }
+  Mail.defaults { delivery_method :smtp, smtp }
+  mail = Mail.new do
+    from "Avvo MH Sync Checker <autoforager.avvo@gmail.com>"
+    to ["18582487349@163.com","yegang.avvo@gmail.com","hewang.cs@gmail.com"]
+    subject "[#{Date.today.strftime('%Y-%m-%d')}] Daily report for failed sync"
+    html_part do
+      content_type 'text/html; charset=UTF-8'
+      # body set_css + get_worker_log_table + get_request_table
+      body set_css + get_worker_log_table + get_request_table
+      # body set_css + get_request_table
+    end
+  end
+  mail.deliver!
+end
+
+def cp_file_from_pod_to_local
+  puts 'cp_file_from_pod begin'
+  system("
+    cd /home/yuhang/.kube;
+    kubectl config set-context --current --namespace=production;
+    kubectl cp martindale-interfaces-worker-foraged-data-to-mh-8ffdd9dc8-ks8vv:/srv/martindale-interfaces/log/foraged_data_to_mh_ec2.log /home/yuhang/.kube/yh/foraged_data_to_mh_ec2.log;
+    ")
+  puts 'cp_file_from_pod end'
+end
+
+def get_worker_log_table
+  h = {}
+  WORKER_LOG_FILE_NAME.each do |file_name|
+    pg_no_connection_error_count = 0
+    File.open(BASE_LOG_PATH + file_name).each do |line|
+      pg_no_connection_error_count += 1 if line =~ /PG::UnableToSend: no connection to the server/
+    end
+    h[file_name] = ['PG::UnableToSend: no connection to the server', pg_no_connection_error_count] if pg_no_connection_error_count != 0
+  end
+  return set_work_log_table(h)
+end
+
+def set_work_log_table(objects)
+  tbody = set_work_log_tbody(objects)
+  return fill_out_worker_log_table(tbody)
+end
+
+def set_work_log_tbody(objects)
+  return if objects.empty?
+  tbody = ""
+  objects.each do |k,v|
+    tbody.concat("<tr><td>#{k}</td><td>#{v[0]}</td><td>#{v[1]}</td></tr>")
+  end
+  return tbody
+end
+
+def fill_out_worker_log_table(tbody)
+  return "<h2>Work Log</h2><p>N/A</p>" if tbody.nil?
+  return <<-EOF
+  <h2>Worker Log</h2>
+  <table class="rtable" border=1>
+    <thead>
+      <tr>
+        <th>LOG NAME</th>
+        <th>FAILED TYPE</th>
+        <th>COUNT</th>
+      </tr>
+    </thead>
+    <tbody>
+    #{tbody}
+    </tbody>
+  </table>
+  EOF
+end
+
+def get_request_table
+  [LawyerMatchRequestLog, LmbRequest, ForagerRequest].map{|model| set_request_table(model) }.join
+end
+
+def set_request_table(klass)
+  sql_statement = if klass == LawyerMatchRequestLog
+    "message like 'failed%' and updated_at > '#{1.day.ago}'"
+  else
+    "status = 'failed' and updated_at > '#{1.day.ago}'"
+  end
+  objects = klass.where(sql_statement).order("id")
+  tbody = set_request_tbody(objects)
+  return fill_out_request_table(klass, tbody)
+end
+
+def set_request_tbody(objects)
+  return if objects.empty?
   tbody = ""
   h = {}
   objects.each do |o|
@@ -48,12 +145,31 @@ def set_tbody(objects)
       h[o.failed_type] = [1, [o.id]]
     end
   end
-  return if h.empty?
+  # return if h.empty?
   h = h.sort_by {|k,v| [v[0], k]}.reverse.to_h
   h.each do |k,v|
-    tbody.concat("<tr><td>#{k}</td><td>#{v[0]}</td><td>#{v[1].size < 3 ? v[1].join('/') : v[1][0,3].join('/') + '/...'}</td></tr>")
+    tbody.concat("<tr><td>#{k}</td><td>#{v[0]}</td><td>#{v[1].size < 4 ? v[1].join('/') : v[1][0,3].join('/') + '/...'}</td></tr>")
   end
   return tbody
+end
+
+def fill_out_request_table(title, tbody = nil)
+  return "<h2>#{title.to_s}</h2><p>N/A</p>" if tbody.nil?
+  return <<-EOF
+    <h2>#{title.to_s}</h2>
+    <table class="rtable" border=1>
+      <thead>
+        <tr>
+          <th>FAILED TYPE</th>
+          <th>COUNT</th>
+          <th>ID</th
+        </tr>
+      </thead>
+      <tbody>
+      #{tbody}
+      </tbody>
+    </table>
+  EOF
 end
 
 def set_css
@@ -73,99 +189,6 @@ def set_css
       a {color: #ff6680;}
       code {background: #fffbcc;font-size: 12px;}
     </style>"
-end
-
-def set_table(klass)
-  objects = klass.where("status = 'failed' and updated_at > '#{9.day.ago}'")
-  tbody = set_tbody(objects)
-  return "<h2>#{klass.to_s}</h2><p>N/A</p>" if tbody.nil?
-  <<-EOF
-    <h2>#{klass.to_s}</h2>
-    <table class="rtable">
-      <thead>
-        <tr>
-          <th>FAILED TYPE</th>
-          <th>COUNT</th>
-          <th>ID</th
-        </tr>
-      </thead>
-      <tbody>
-      #{tbody}
-      </tbody>
-    </table>
-  EOF
-end
-
-def set_body
-  str_body = ""
-  [LawyerMatchRequest, LmbRequest, ForagerRequest].map{
-    |model| 
-    str_body += set_table(model)
-  }
-  return set_css + str_body
-end
-
-def cp_file_from_pod
-  puts 'cp_file_from_pod begin'
-  system("
-    cd /home/dyson/.kube;
-    kubectl config set-context --current --namespace=und-618;
-    kubectl cp martindale-interfaces-worker-foraged-data-to-mh-8ffdd9dc8-ks8vv:/srv/martindale-interfaces/log/foraged_data_to_mh_ec2.log /home/dyson/.kube/yh/foraged_data_to_mh_ec2.log;
-    ")
-  puts 'cp_file_from_pod end'
-end
-
-def set_log
-  cp_file_from_pod
-  debug_count = 0
-  pg_no_connection_error_count = 0
-  File.open('/home/dyson/.kube/yh/foraged_data_to_mh_ec2.log').each do |line|
-    debug_count += 1 if line =~ /^D,/i
-    pg_no_connection_error_count += 1 if line =~ /PG::UnableToSend: no connection to the server/
-  end
-  return "<h2>Foraged Data To Mh Log</h2><p>N/A</p>"
-  return <<-EOF
-    <h2>Foraged Data To Mh Log</h2>
-    <table class="rtable">
-      <thead>
-        <tr>
-          <th>FAILED TYPE</th>
-          <th>COUNT</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr>
-      <td>PG::UnableToSend: no connection to the server</td>
-      <td>#{pg_no_connection_error_count}</td>
-      <td>debug line</td>
-      <td>#{debug_count}</td>
-      <tr>
-      </tbody>
-    </table>
-  EOF
-end
-
-def send_mail
-  smtp = { 
-    :address => "smtp.gmail.com",
-    :port => 587, 
-    :domain => "gmail.com",
-    :user_name => "autoforager.avvo@gmail.com",
-    :password => "avvo.com2",
-    :enable_starttls_auto => true
-  }
-  Mail.defaults { delivery_method :smtp, smtp }
-  mail = Mail.new do
-    from "autoforager.avvo@gmail.com"
-    # to ["18582487349@163.com","yegang.avvo@gmail.com","hewang.cs@gmail.com"]
-    to ["18582487349@163.com"]
-    subject "[#{Date.today.strftime('%Y-%m-%d')}] Daily report for failed sync"
-    html_part do
-      content_type 'text/html; charset=UTF-8'
-      body set_log + set_body
-    end
-  end
-  mail.deliver!
 end
 
 send_mail
